86dd7322efcf528656d06b09b73505269ab8ff93
sabuj37 <89691756+sabuj37@users.noreply.github.com>
2025-05-19 18:51:45 +0600

    profit

--- resources/views/sale/newsale.blade.php
+++ resources/views/sale/newsale.blade.php
@@ -3,7 +3,7 @@
     <div class="col-md-12 col-12">
         <div class="card">
             <div class="card-body">
-                <div class="row">
+                <div class="row align-items-center">
                     <div class="col-md-3">
                         <div class="form-group">
                             <label>Select Customer *</label>
@@ -14,28 +14,27 @@
                             </select>
                         </div>
                     </div>
-                    <div class="col-md-1 mt-5 p-0">
-                        <button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#brand">customer</button>
+                    <div class="col-md-1 mt-3 p-0 ">
+                        <button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#customer"><i class="las la-plus mr-2"></i>customer</button>
                     </div>
 
-                    
-                        <div class="col-md-3">
-                            <div class="form-group">
-                                <label>Date</label>
-                                <input type="date" class="form-control" placeholder="" required />
-                                <div class="help-block with-errors"></div>
-                            </div>
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label>Date</label>
+                            <input type="date" class="form-control" placeholder="" required />
+                            <div class="help-block with-errors"></div>
                         </div>
+                    </div>
 
-                        <div class="col-md-3">
-                            <div class="form-group">
-                                <label>Reference *</label>
-                                <input type="text" class="form-control" placeholder="Enter Name" required />
-                                <div class="help-block with-errors"></div>
-                            </div>
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label>Reference *</label>
+                            <input type="text" class="form-control" placeholder="Enter Name" required />
+                            <div class="help-block with-errors"></div>
                         </div>
-                         <div class="col-md-1 mt-5 p-0">
-                        <button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#brand">Profit</button>
+                    </div>
+                    <div class="col-md-1 mt-3 p-0 ">
+                        <button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#customer">Profit</button>
                     </div>
                 </div>
             </div>
@@ -43,4 +42,58 @@
     </div>
 </div>
 
+<!-- start_model -->
+ <div class="modal fade" id="customer" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="customer" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6 class="modal-title fs-5" id="customer">Creat Customer</h6>
+                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <label for="formGroupExampleInput" class="form-label">Name</label>
+                                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Enter Category name" />
+                                    </div>
+
+                                    <div class="col-4">
+                                        <label for="mobile" class="form-label">Mobile</label>
+                                        <input type="text" class="form-control" id="mobile" placeholder="Enter Category name" />
+                                    </div>
+
+                                    <div class="col-4">
+                                        <label for="formGroupExampleInput" class="form-label">Name</label>
+                                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Enter Category name" />
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-8 mt-2">
+                                        <label for="addres" class="form-label">Addrass</label>
+                                        <input type="Address" class="form-control" id="Address" placeholder="Give Address" />
+                                    </div>
+
+                                    <div class="col-4 mt-2">
+                                        <label for="email" class="form-label">Email</label>
+                                        <input type="email" class="form-control" id="email" placeholder="Enter your Email" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-light" data-dismiss="modal">Cancle</button>
+                        <button type="button" class="btn btn-primary">Save</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- end_model -->
 @endsection
